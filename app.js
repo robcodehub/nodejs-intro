78b938cb2d49571ece88d6f86e6db1133af41ba7
Robert Wise <robcodehub@gmail.com>
2019-09-08 22:32:53 +0200

    added users.json file, created promise for fs readfile

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("hello from node")
+const fs = require('fs');
+
+const readFile = (file) => {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data) => {
+      if(err) {
+        reject(err);
+      }
+      else {
+        resolve(data.toString())
+      }
+    }
+  )}
+  )};
+
+
+readFile('./users.json')
+.then( data => console.log('resolve', data))
+.catch( ex => console.log('rejected', ex));
+
+
