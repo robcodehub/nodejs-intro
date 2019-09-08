2319159bdbd46660952f604cc12de33eb6f7b403
Robert Wise <robcodehub@gmail.com>
2019-09-09 00:19:14 +0200

    add new user on page reload added

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const readFile = (file) => {
     }
   )}
   )};
- 
+
   const writeFile = (file, data) => {
     return new Promise((resolve, reject)=> {
       fs.writeFile(file, data, (err, data) => {
@@ -29,24 +29,66 @@ const readFile = (file) => {
     )})
   };
 
+ const addUser = (user) => {
+   return readFile('./users.json')
+   .then(data => {
+     const users = JSON.parse(data);
+     let max = users.reduce((acc, user)=> {
+       if(user.id > acc) {
+         acc = user.id;
+       }
+       return acc;
+     }, 0);
+     user.id = max + 1;
+     users.push(user);
+     return writeFile('./users.json', JSON.stringify(users));
+   })
+   .then(() => {
+     return user;
+   })
+ };
 
-writeFile('./users.json', JSON.stringify([{ id: 1, name: 'moe' }]))
-.then(() => console.log('success'))
-.catch(() => console.log('failure'));
+
+addUser({ name: `curly-${Math.random()}`})
+.then( user => console.log(user));
+
 
 
 http.createServer((req, res)=> {
 if (req.url === '/api/users') {
-  readFile('./users.json')
-  .then(data => {
-    res.write(data);
-    res.end();
-  })
-  .catch( ex => {
-    res.statusCode = 500;
-    res.write(ex.message);
-    res.end();
-  })
+    if(req.method === 'GET') {
+    readFile('./users.json')
+    .then(data => {
+      res.write(data);
+      res.end();
+    })
+    .catch( ex => {
+      res.statusCode = 500;
+      res.write(ex.message);
+      res.end();
+    })
+  }
+  else {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', ()=> {
+      const user = JSON.parse(body);
+      addUser(user)
+      .then(user => {
+        res.statusCode = 201;
+        res.write(JSON.stringify(user));
+        res.end();
+      })
+      .catch(ex => {
+        res.statusCode = 500;
+        res.write(ex.message);
+        res.end();
+      })
+      console.log(user);
+    })
+  }
 } else if (req.url === '/') {
     readFile('./index.html')
     .then(data => {
