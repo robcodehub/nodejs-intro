904401a3f7b0be6f38791cbfc50c42b36391ecd6
Robert Wise <robcodehub@gmail.com>
2019-09-08 23:06:17 +0200

    sending JSON data from server

--- app.js
+++ app.js
@@ -17,8 +17,21 @@ const readFile = (file) => {
   )};
 
 http.createServer((req, res)=> {
+if (req.url === '/api/users') {
+  readFile('./users.json')
+  .then(data => {
+    res.write(data);
+    res.end();
+  })
+  .catch( ex => {
+    res.statusCode = 500;
+    res.write(ex.message);
+    res.end();
+  })
+} else {
   res.write('hello world!');
   res.end();
+}
 }).listen(3000);
 
 
